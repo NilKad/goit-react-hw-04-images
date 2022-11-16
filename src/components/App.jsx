3d3a991ac29bb61817201012a4ec7270929dd628
NilKad <kadulin@gmail.com>
2022-11-16 02:36:33 +0200

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,28 +25,8 @@ export default function App() {
     window.scrollTo(0, 0);
   };
 
-  // const galleryFilter = list => {
-  //   return list.map(({ id, webformatURL, tags, largeImageURL }) => ({
-  //     id,
-  //     webformatURL,
-  //     tags,
-  //     largeImageURL,
-  //   }));
-  // };
-
-  // const addItemGallery = galleryList => {
-  //   page === 1
-  //     ? setGallery(galleryFilter(galleryList))
-  //     : setGallery(prev => [...prev, ...galleryFilter(galleryList)]);
-  // };
-
-  // const setTotalPage = total => this.setState({ totalPage: total });
-
   const onClickButtonLoadMore = () => {
     setPage(p => p + 1);
-    // this.setState(prevState => ({
-    //   page: prevState.page + 1,
-    // }));
     console.log('load more...');
   };
 
@@ -55,60 +35,28 @@ export default function App() {
     setModalItem(item);
   };
 
-  // const modalClose = e => setModalItem(null);
-
-  // this.setState({ modalItem: null });
   const loadingFinish = e => {
     isLoading && setIsLoading(false);
-    // this.setState({ isLoading: false });
   };
 
   useEffect(() => {
     if (searchString !== '') {
       setIsLoading(true);
-      // const startFetch = Date.now();
-      // console.log('start fetch date: ', startFetch);
       fetchApi({
         searchString: searchString,
         page: page,
         setTotalPage: setTotalPage,
         setGallery: setGallery,
       }).finally(() => {
-        // console.log('finale fetch date: ', Date.now() - startFetch, 'ms');
         setIsLoading(false);
       });
     }
-    // gallery.length === 0 && setIsLoading(false);
   }, [page, searchString]);
 
   useEffect(() => {
     page === totalPage ? setIsHaveLoadMore(false) : setIsHaveLoadMore(true);
   }, [page, totalPage]);
 
-  // };
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { searchString, page, totalPage, gallery } = this.state;
-  //   if (prevState.searchString !== searchString || prevState.page !== page) {
-  //     this.setState({ isLoading: true });
-  //     fetchApi({
-  //       searchString: searchString,
-  //       page: page,
-  //       setTotalPage: this.setTotalPage,
-  //       addItemGallery: this.addItemGallery,
-  //     });
-  //   }
-  //   if (prevState.page !== page || prevState.totalPage !== totalPage) {
-  //     page === totalPage
-  //       ? this.setState({ isHaveLoadMore: false })
-  //       : this.setState({ isHaveLoadMore: true });
-  //   }
-  //   if (gallery.length === 0 && prevState.gallery !== gallery) {
-  //     this.setState({ isLoading: false });
-  //   }
-  // }
-
-  // render() {
-  // const { gallery, isHaveLoadMore, modalItem, isLoading } = this.state;
   return (
     <section className={css.App}>
       <Searchbar seacrhSubmit={searchSubmit} />
